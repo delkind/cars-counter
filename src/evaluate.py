651bdfa69db38af7f7852ea1d4777b5b156460c2
David Elkind <david.elkind@gmail.com>
2019-02-10 16:48:22 +0200

    Fitting confidence threshold fixes

--- src/evaluate.py
+++ src/evaluate.py
@@ -90,8 +90,8 @@ def fit_confidence_threshold(model, dataset_root, validation_set, preprocess_ima
     if validation_set is None:
         raise Exception("Error: validation set is required to fit the confidence threshold")
 
-    print("Fitting confidence threshold...", end='')
     eval_set = CarsDataset(dataset_root, validation_set=validation_set)
+    print("Fitting confidence threshold, {} items in validation set...".format(len(validation_set)), end='')
     preds = list(map(lambda tup: predict_image(model, tup[0], tup[1], preprocess_image=preprocess_image),
                      eval_set.validation.items()))
     errors_by_conf = [(confidence / 100, np.array([abs(len([p for p in pred if p[1] > confidence / 100]) - len(gt))
